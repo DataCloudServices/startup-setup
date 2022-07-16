e68a40bb53c44d37cec28ce2c77e0198d3134e37
rafidini <rafidinarivo.study@gmail.com>
2022-07-16 23:36:37 +0200

    2nd commit

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# ---- Variables ---- 
+dcs_startup_name="DataCloudServices"
+dcs_onedev_tag="7.4.6"
+dcs_path_root="~/${dcs_startup_name}"
+dcs_path_config="${dcs_path_root}/config"
+dcs_url_dockercompose="https://github.com/DataCloudServices/startup-setup/blob/main/docker-compose.yaml"
+
+# ---- INIT ----
+mkdir "~/${dcs_startup_name}"
+mkdir ${dcs_path_config}
+
+# ---- OneDev -----
+docker run \
+    --tty \ 
+    --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ${dcs_path_config}/onedev:/opt/onedev \
+    -p 6610:6610 \
+    -p 6611:6611 \
+    --detach \
+    1dev/server:${dcs_onedev_tag}
+
+# ---- WikiJs
+cd ${dcs_path_config}
+wget ${dcs_url_dockercompose}
+docker-compose up -d
+cd -
+
